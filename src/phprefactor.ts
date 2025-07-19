a1563deea92c5007018a83dc3e0910884652c7dc
Michiel Nijenhuis <michiel.nijenhuis@gmail.com>
2025-07-19 17:37:49 +0200

    fix: add missing use statement in generated rector config

--- src/phprefactor.ts
+++ src/phprefactor.ts
@@ -148,6 +148,7 @@ export class PHPRefactorManager {
 declare(strict_types=1);
 
 use Rector\\Config\\RectorConfig;
+use Rector\\Set\\ValueObject\\SetList;
 
 $config = RectorConfig::configure()
     ->withPaths([
